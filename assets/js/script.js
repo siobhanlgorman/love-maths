8fa027b9c8493413abfc87783db7435dae0ee685
siobhanlgorman <siobhanlgorman@gmail.com>
2021-06-08 14:38:44 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer () {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
+function displayDivisionQuestion(params) {
+    
+}
